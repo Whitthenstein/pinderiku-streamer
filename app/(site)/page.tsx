96c06d720a940dd7c4ed182e4b09e2e5ef8f7f93
João Branco <joaombranco92@gmail.com>
2023-09-24 19:00:57 +0100

    remove liked songs for unauthenticated user

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import PageContent from "./components/PageContent";
 import Image from "next/image";
+import LikedSongs from "@/components/LikedSongs";
 
 export const revalidate = 0;
 
@@ -41,22 +42,7 @@ const Home = async () => {
               Welcome back to Streamer!
             </h1>
           </div>
-          <div
-            className="grid
-            grid-cols-1
-            sm:grid-cols-2
-            xl:grid-cols-3
-            2xl:grid-cols-4
-            gap-3
-            mt-4
-          "
-          >
-            <ListItem
-              image="/images/liked.png"
-              name="Liked Songs"
-              href="liked"
-            />
-          </div>
+          <LikedSongs />
         </div>
       </Header>
       <div className="mt-2 mb-7 px-6">
---
+++ components/LikedSongs.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useUser } from "@/hooks/useUser";
+import ListItem from "./ListItem";
+
+const LikedSongs = () => {
+  const { user } = useUser();
+  return (
+    <div
+      className="grid
+        grid-cols-1
+        sm:grid-cols-2
+        xl:grid-cols-3
+        2xl:grid-cols-4
+        gap-3
+        mt-4
+      "
+    >
+      {user?.id && (
+        <ListItem
+          image="/images/liked.png"
+          name="Liked Songs"
+          href="liked"
+        />
+      )}
+    </div>
+  );
+};
+
+export default LikedSongs;
