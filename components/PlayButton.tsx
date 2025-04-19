24abcf71406f4b8104795f60fe197cd37fb3e704
João Branco <joaombranco92@gmail.com>
2025-04-19 15:41:22 +0100

    add more e2e testing ids

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -17,6 +17,7 @@ const PlayButton: React.FC<PlayButtonProps> = ({ songId }) => {
 
   return (
     <button
+      data-testid="song-play-button"
       onClick={handleClick}
       className="translate flex translate-y-1/4 items-center rounded-full bg-white p-4 opacity-0 drop-shadow-md transition hover:scale-110 group-hover:translate-y-0 group-hover:opacity-100"
     >
